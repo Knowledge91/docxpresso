90a5080df797fb5136135a68815e05824a7e70f3
knowledge91 <dirkhornung91@gmail.com>
2018-08-10 11:46:09 +0200

    added API KEY

--- index.js
+++ index.js
@@ -5,35 +5,38 @@ const Base64 = require('js-base64').Base64;
 const hex2bin = require('locutus/php/strings/hex2bin');
 const crypto = require('crypto');
 
-const masterKey = process.env.APIKEY;
+const masterKey = process.env.DOCX_API_KEY;
 const docxpressoInstallation = 'https://cysae.a.docxpresso.com';
 
 function returnLink(url, id, opt) {
-    const uuid = uuidv4().replace(/-/g, '');
-    const timestamp = Math.floor(new Date().getTime() / 1000);
+  const uuid = uuidv4().replace(/-/g, '');
+  const timestamp = Math.floor(new Date().getTime() / 1000);
 
-    let control = ''
-    control += id + '-';
-    control += timestamp + '-' + uuid;
-    control += '-' + opt;
+  let control = ''
+  control += id + '-';
+  control += timestamp + '-' + uuid;
+  control += '-' + opt;
 
-    const dataKey = crypto.createHash('sha1').update(control).digest();
-    const apiKey = crypto.createHmac('sha1', masterKey).update(dataKey).digest('hex');
-    console.log(apiKey);
+  const dataKey = crypto.createHash('sha1').update(control).digest();
+  const apiKey = crypto.createHmac('sha1', masterKey).update(dataKey).digest('hex');
 
-    let addr = `${url}?uniqid=${uuid}&timestamp=${timestamp}&APIKEY=${apiKey}&options=${opt}`;
+  let addr = `${url}?uniqid=${uuid}&timestamp=${timestamp}&APIKEY=${apiKey}&options=${opt}`;
 
-    return addr;
+  return addr;
 }
 
 
-export function previewDocument(id) {
-    let url = `${docxpressoInstallation}/documents/preview/${id}`;
+function previewDocument(id, responseURL = '', responseDataURI = '') {
+  let url = `${docxpressoInstallation}/documents/preview/${id}`;
 
-    const opt = {
-        'format': 'odt'
-    }
+  const opt = {
+    format: 'odt',
+    responseURL,
+    responseDataURI,
+  }
 
-    const optBase64 = Base64.encodeURI(JSON.stringify(opt));
-    return returnLink(url, id, optBase64+',,');
+  const optBase64 = Base64.encodeURI(JSON.stringify(opt));
+  return returnLink(url, id, optBase64+',,');
 }
+
+module.exports.previewDocument = previewDocument
---
+++ test.js
@@ -0,0 +1,3 @@
+const docx = require('./index.js')
+
+console.log(docx.previewDocument(115, 'http://localhost:3000/', 'https://5fmz4wdy17.execute-api.eu-west-1.amazonaws.com/dev/meeting/convene'))
